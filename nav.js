b34a36b82155f7870dbf90acd00458850c9b7df7
alanosauras <bandbkitboats@gmail.com>
2024-03-13 19:04:41 -0400

    fixed charts missing

--- nav.js
+++ nav.js
@@ -4,6 +4,7 @@ document.getElementById('navigation').innerHTML = `
     <nav>
         <ul>
             <li><a href="index.html">Lookup</a></li>
+            <li><a href="classchart.html">Charts</a></li>
             <li><a href="ww.html">Wilderness Waterway</a></li>
             <li><a href="boatdata.html">Boat Data</a></li>
             <li><a href="missingboatinfo.html">Missing Info</a></li> 
