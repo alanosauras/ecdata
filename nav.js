cce0fc40b719a6f7cefb2b36b8426dfe93a07c4e
alanosauras <bandbkitboats@gmail.com>
2024-03-13 07:44:24 -0400

    upgraded nav

---
+++ nav.js
@@ -0,0 +1,10 @@
+document.getElementById('navigation').innerHTML = `
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="boatdata.html">Boat Data</a></li>
+            <li><a href="classchart.html">Charts</a></li>
+            <!-- <li><a href="triberdata.html">Triber Data</a></li> -->
+        </ul>
+    </nav>
+`;
