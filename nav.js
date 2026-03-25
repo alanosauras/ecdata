6731e0c4df94f7a5c658b387efff99c635d7ceaf
alanosauras <77589548+alanosauras@users.noreply.github.com>
2026-03-25 13:28:27 -0400

    Add files via upload

--- nav.js
+++ nav.js
@@ -6,12 +6,12 @@ document.getElementById('navigation').innerHTML = `
             <li><a href="index.html">Lookup</a></li>
             <li><a href="ww.html">Wilderness Waterway</a></li>
             <li><a href="records.html">Stats/Records</a></li>
+            <li><a href="other.html">Other Stats</a></li>
             <li><a href="classchart.html">Charts</a></li>
             <li><a href="boatdata.html">Boat Data</a></li>
             <li><a href="missingboatinfo.html">Missing Info</a></li> 
             <li><a href="cursedboats.html">Cursed Boats?</a></li> 
             <li><a href="lucky.html">Lucky Tribers</a></li>
-            <li><a href="other.html">Other</a></li>
             <li><a href="notes.html">About</a></li> 
             <li><a href="ERRORS.html">Erroneous Data</a></li>
 
